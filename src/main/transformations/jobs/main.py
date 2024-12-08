4099c7a08afaf1ddf40d59cd292d09f8a5fbb65c
YashChauhan <y4shchauhan@gmail.com>
2024-12-09 01:59:24 +0530

    implement module to connect to mysql and check the staging table

--- src/main/transformations/jobs/main.py
+++ src/main/transformations/jobs/main.py
@@ -1,6 +1,7 @@
 from urllib import response
 from resources.dev import config
 from src.main.utility.encrypt_decrypt import *
+from src.main.utility.my_sql_session import get_mysql_connection
 from src.main.utility.s3_client_object import *
 from src.main.utility.logging_config import *
 
@@ -17,3 +18,33 @@ s3_client = s3_client_provider.get_client()
 response = s3_client.list_buckets()
 print(response)
 logger.info("List of Buckets: %s", response["Buckets"])
+
+
+# Check if local directory already has a file
+# if file is there then check if the same file is present in the staging area
+# with status as A. If so then don't delete and try to rerun else give error.
+
+csv_files = [file for file in os.listdir(
+    config.local_directory) if file.endswith(".csv")]
+connection = get_mysql_connection()
+cursor = connection.cursor()
+
+total_csv_files = []
+if csv_files:
+    for file in csv_files:
+        total_csv_files.append(file)
+
+    statement = f"""
+        select distinct file_name from
+        {config.database_name}.{config.product_staging_table}
+        where file_name in ({str(total_csv_files)[1:-1]}) and status='I
+    """
+    logger.info(f"dynamically created statement: {statement}")
+    cursor.execute(statement)
+    data = cursor.fetchall()
+    if data:
+        logger.info("Your last run was failed, please check!!!")
+    else:
+        logger.info("No record Found")
+else:
+    logger.info("last run was successfull!!!")
