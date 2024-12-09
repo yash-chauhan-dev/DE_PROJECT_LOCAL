5fbe15d03d4f45bc791d20a406904cde8aeede66
YashChauhan <y4shchauhan@gmail.com>
2024-12-09 14:00:19 +0530

    Implemented logic to download data from s3 and save it to localdirectory

--- src/main/transformations/jobs/main.py
+++ src/main/transformations/jobs/main.py
@@ -1,5 +1,8 @@
+from threading import local
 from urllib import response
 from resources.dev import config
+from src.main.download.aws_file_download import S3FileDownloader
+from src.main.read.aws_read import S3Reader
 from src.main.utility.encrypt_decrypt import *
 from src.main.utility.my_sql_session import get_mysql_connection
 from src.main.utility.s3_client_object import *
@@ -48,3 +51,65 @@ if csv_files:
         logger.info("No record Found")
 else:
     logger.info("last run was successfull!!!")
+
+
+# Read files from s3 bucket and download to local directory
+try:
+    s3_reader = S3Reader()
+
+    folder_path = config.s3_source_directory
+    s3_absolute_file_path = s3_reader.list_files(
+        s3_client,
+        config.bucket_name,
+        folder_path=folder_path
+    )
+    logger.info("Absolute path on s3 bucket for csv file %s",
+                s3_absolute_file_path)
+    if not s3_absolute_file_path:
+        logger.info(f"No files available at {folder_path}")
+        raise Exception("No Data available to process")
+
+except Exception as e:
+    logger.error("Exited with error: %s", e)
+    raise e
+
+bucket_name = config.bucket_name
+local_directory = config.local_directory
+
+prefix = f"s3://{bucket_name}/"
+file_paths = [url[len(prefix):] for url in s3_absolute_file_path]
+logging.info("File path available on s3 under %s bucket and folder name is %s",
+             bucket_name, file_paths)
+
+try:
+    downloader = S3FileDownloader(s3_client, bucket_name, local_directory)
+    downloader.download_files(file_paths)
+
+except Exception as e:
+    logger.error("File download error: %s", e)
+    sys.exit()
+
+# Get list of all files in the local directory
+all_files = os.listdir(local_directory)
+logger.info(
+    f"List of files present at my local directory after download {all_files}")
+
+# Filter files with .csv in their names amnd create absolute paths
+if all_files:
+    csv_files = []
+    error_files = []
+    for files in all_files:
+        if files.endswith(".csv"):
+            csv_files.append(os.path.abspath(
+                os.path.join(local_directory, files)))
+        else:
+            error_files.append(os.path.abspath(
+                os.path.join(local_directory, files)))
+
+    if not csv_files:
+        logger.error("No csv data available to process the request")
+        raise Exception("No csv data available to process the request")
+
+else:
+    logger.error("There is no data to process")
+    raise Exception("There is no data to process")
