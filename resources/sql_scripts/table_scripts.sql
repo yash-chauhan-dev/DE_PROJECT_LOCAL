611ce4d91112a8d764656bb1daf188b157a4c856
YashChauhan <y4shchauhan@gmail.com>
2024-12-10 16:17:53 +0530

    MySQL Table creation scripts

--- resources/sql_scripts/table_scripts.sql
+++ resources/sql_scripts/table_scripts.sql
@@ -6,3 +6,77 @@ CREATE TABLE product_staging_table (
     updated_date TIMESTAMP ,
     status VARCHAR(1)
 );
+
+CREATE TABLE customer (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    address VARCHAR(255),
+    pincode VARCHAR(10),
+    phone_number VARCHAR(20),
+    customer_joining_date DATE
+);
+
+--store table
+CREATE TABLE store (
+    id INT PRIMARY KEY,
+    address VARCHAR(255),
+    store_pincode VARCHAR(10),
+    store_manager_name VARCHAR(100),
+    store_opening_date DATE,
+    reviews TEXT
+);
+
+-- product table
+CREATE TABLE product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    current_price DECIMAL(10, 2),
+    old_price DECIMAL(10, 2),
+    created_date TIMESTAMP ,
+    updated_date TIMESTAMP ,
+    expiry_date DATE
+);
+
+--sales team table
+CREATE TABLE sales_team (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    manager_id INT,
+    is_manager CHAR(1),
+    address VARCHAR(255),
+    pincode VARCHAR(10),
+    joining_date DATE
+);
+
+--s3 bucket table
+CREATE TABLE s3_bucket_info (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    bucket_name VARCHAR(255),
+    file_location VARCHAR(255),
+    created_date TIMESTAMP ,
+    updated_date TIMESTAMP ,
+    status VARCHAR(20)
+);
+
+--Data Mart customer
+CREATE TABLE customers_data_mart (
+    customer_id INT ,
+    full_name VARCHAR(100),
+    address VARCHAR(200),
+    phone_number VARCHAR(20),
+    sales_date_month DATE,
+    total_sales DECIMAL(10, 2)
+);
+
+
+--sales mart table
+CREATE TABLE sales_team_data_mart (
+    store_id INT,
+    sales_person_id INT,
+    full_name VARCHAR(255),
+    sales_month VARCHAR(10),
+    total_sales DECIMAL(10, 2),
+    incentive DECIMAL(10, 2)
+);
